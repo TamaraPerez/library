c952b74cc489ae8de95d7af4dda6cbd33b7697ad
Tamara Perez <tamaraa.91@gmail.com>
2021-04-12 00:17:08 +0200

    Make aggregate root concept easier (as a annotation instead of abstract class)

--- src/main/kotlin/edu/salleurl/context/catalog/books/domain/Book.kt
+++ src/main/kotlin/edu/salleurl/context/catalog/books/domain/Book.kt
@@ -2,7 +2,8 @@ package edu.salleurl.context.catalog.books.domain
 
 import edu.salleurl.context.shared.domain.aggregate.AggregateRoot
 
-data class Book(val id: BookId, val name: Name, val author: Author): AggregateRoot() {
+@AggregateRoot
+data class Book(val id: BookId, val name: Name, val author: Author) {
     companion object {
         fun create(id: BookId, name: Name, author: Author) = Book(id, name, author)
     }
