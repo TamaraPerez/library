fcca70eb528946a9eb497f2fd621c68d223fedc9
Tamara Perez <tamaraa.91@gmail.com>
2021-04-28 22:27:46 +0200

    Inline

--- src/main/kotlin/edu/salleurl/context/catalog/books/application/BookCreatorUseCase.kt
+++ src/main/kotlin/edu/salleurl/context/catalog/books/application/BookCreatorUseCase.kt
@@ -8,10 +8,7 @@ import edu.salleurl.context.shared.domain.bus.publisher.EventPublisher
 import javax.inject.Named
 
 @Named
-class BookCreatorUseCase(
-    private val creator: BookCreator,
-    private val publisher: EventPublisher
-) {
+class BookCreatorUseCase(private val creator: BookCreator, private val publisher: EventPublisher) {
 
     fun execute(id: String, name: String, author: String) {
         creator.invoke(BookId(id), Name(name), Author(author))
